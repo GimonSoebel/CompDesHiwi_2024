e5632e8763b7510be0b42b52aa32caae8c29a8ad
GimonSoebel <st189076@stud.uni-stuttgart.de>
2024-11-04 14:24:27 +0100

    Remove old volume calculator script and add new Python examples

---
+++ VSCode/class_29_10_24.py
@@ -0,0 +1,41 @@
+"""
+my_list = ["bla"]
+another_list = [1,2,3, "something"]
+print(another_list)
+"""
+
+"""
+heroes = ["Batman", "Wolverine", "Superman"]
+print(heroes)
+heroes.append("Ant Man")
+print(heroes)
+heroes.pop(1)
+print(heroes)
+heroes.sort()
+print(heroes)
+subheroes = heroes[1:2]
+print(subheroes)
+if "Wolverine" in heroes:
+    wolverine_index = heroes.index("Wolverine")
+    print(wolverine_index)
+    print("Wolverine is in the list at index No.{}".format(wolverine_index))
+"""
+
+"""
+set1 = {1,2,3}
+set2 = {3,4,5}
+union_set = set1 | set2
+intersection_set = set1 & set2
+difference_set = set1 - set2
+print(union_set)
+print(intersection_set)
+print(difference_set)
+"""
+
+"""
+my_list = [1,2]
+my_tuple = ([3,2],my_list)
+my_list[1] = 0
+print(my_tuple)
+"""
+
